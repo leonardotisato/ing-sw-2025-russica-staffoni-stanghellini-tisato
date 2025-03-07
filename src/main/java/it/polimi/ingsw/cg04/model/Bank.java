7f8ad5fe78e763220a9734c1f281ee9615ebc74e
Filippo <filippostaffoni@hotmail.it>
2025-03-07 17:09:29 +0100

    Bank, Game and FlightBoard classes added, still to be completed

---
+++ src/main/java/it/polimi/ingsw/cg04/model/Bank.java
@@ -0,0 +1,61 @@
+package it.polimi.ingsw.cg04;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Bank {
+    public int numCredits;
+    public int numBatteries;
+    public Map<boxType, Integer> boxes;
+    public int numHumans;
+    public int numAliens;
+
+    private Bank(int numCredits, int numBatteries, int numHumans, int numAliens, Map<boxType, Integer> boxes) {
+        this.numCredits = numCredits;
+        this.numBatteries = numBatteries;
+        this.numHumans = numHumans;
+        this.numAliens = numAliens;
+        this.boxes = new HashMap<>(boxes);
+    }
+
+    private void addCredits(int credits){
+        this.numCredits += credits;
+    }
+
+    private void addBatteries(int batteries){
+        this.numBatteries += batteries;
+    }
+
+    private void addHumans(int humans){
+        this.numHumans += humans;
+    }
+
+    private void addAliens(int aliens){
+        this.numAliens += aliens;
+    }
+
+    private int getNumCredits(){
+        return this.numCredits;
+    }
+
+    private int getNumBatteries(){
+        return this.numBatteries;
+    }
+
+    private int getNumHumans(){
+        return this.numHumans;
+    }
+
+    private int getNumAliens(){
+        return this.numAliens;
+    }
+
+    private Map<boxType,Integer> getBoxes(){
+        return this.boxes;
+    }
+
+    private void addBoxes(boxType type, int num){
+        boxes.put(type, boxes.get(type) + num);
+    }
+    
+}
