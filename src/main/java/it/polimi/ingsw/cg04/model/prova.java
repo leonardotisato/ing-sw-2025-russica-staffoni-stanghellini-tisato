667b0a4975e7103a059d63fbe3b4c73fdc5abeca
leonardotisato <10844472@polimi.it>
2025-03-07 13:46:20 +0100

    added tests

---
+++ src/main/java/it/polimi/ingsw/cg04/model/prova.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.cg04.model;
+
+public class prova {
+
+}
---
+++ src/test/java/it/polimi/ingsw/cg04/model/provaTest.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.cg04.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class provaTest {
+
+}
