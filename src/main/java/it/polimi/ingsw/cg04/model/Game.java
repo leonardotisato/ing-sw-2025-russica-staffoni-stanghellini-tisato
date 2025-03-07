299f4d063eef1b678ea4a4918c890f24c6ee6ebe
Daniele Russica <daniele.russica@gmail.com>
2025-03-07 19:40:46 +0100

    fixed list import, typos, imported enums, make it work better with player

--- src/main/java/it/polimi/ingsw/cg04/model/Game.java
+++ src/main/java/it/polimi/ingsw/cg04/model/Game.java
@@ -1,12 +1,18 @@
 package it.polimi.ingsw.cg04.model;
+import it.polimi.ingsw.cg04.model.FlightBoard;
+import it.polimi.ingsw.cg04.model.Player;
+import it.polimi.ingsw.cg04.model.enumerations.PlayerColor;
+import it.polimi.ingsw.cg04.model.enumerations.State;
 
 import java.util.ArrayList;
+import java.util.List;
+
 
 public class Game{
     private int maxPlayers;
     private int numPlayers;
     private List<Player> players;
-    private flightBoard board;
+    private FlightBoard board;
     private State state;
     private Bank bank;
 
@@ -14,7 +20,7 @@ public class Game{
         this.maxPlayers = 0;
         this.numPlayers = 0;
         this.players = new ArrayList<Player>();
-        this.board = new flightBoard();
+        this.board = new FlightBoard();
         this.state = State.START;
     }
 
@@ -25,26 +31,26 @@ public class Game{
 
     private boolean isNameTaken(String name){
         for (Player p : players){
-            if(p.name.equals(name)) return true;
+            if(p.getName().equals(name)) return true;
         }
         return false;
     }
 
-    public void addPlayer(String name, Color color) throws NameAlreadyTakenException{
-        // check name alreadytaken
+    public void addPlayer(String name, PlayerColor color) throws NameAlreadyTakenException{
+        // check name already taken
         if(this.isNameTaken(name)) throw new NameAlreadyTakenException();
-        this.players.add(new Player(name,color));
+        this.players.add(new Player(name, color, this));
     }
 
     public void removePlayer(String name){
         for (Player p : this.players){
-            if(p.name.equals(name)){
+            if(p.getName().equals(name)){
                 this.players.remove(p);
             }
         }
     }
 
-    public void setBoard(flightBoard board){
+    public void setBoard(FlightBoard board){
         this.board = board;
     }
 
@@ -56,20 +62,20 @@ public class Game{
         return numPlayers;
     }
 
-    public flightBoard getBoard() {
+    public FlightBoard getBoard() {
         return board;
     }
 
     public Player getPlayer(String name){
         for (Player p : players){
-            if(p.name.equals(name)) return p;
+            if(p.getName().equals(name)) return p;
         }
         return null;
     }
 
     public Player getPlayer(int position){
         for (Player p : players){
-            if(p.position == position) return p;
+            if(p.getPosition() == position) return p;
         }
         return null;
     }
