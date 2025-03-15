c11c2735aa50641d5f24ed5e0944c937ccd601cf
stanga <gio.stanga@gmail.com>
2025-03-15 11:51:15 +0100

    reorganized Ship

--- src/main/java/it/polimi/ingsw/cg04/model/ships/Ship.java
+++ src/main/java/it/polimi/ingsw/cg04/model/ships/Ship.java
@@ -68,6 +68,8 @@ public class Ship {
         // todo: placeTile(new HousingTile(playerColor), shipHeight / 2, shipWidth / 2);
     }
 
+
+    // ship structure and tiles management
     public Tile[][] getTilesMatrix() {
         return tilesMatrix;
     }
@@ -92,157 +94,6 @@ public class Ship {
 
     public int getNumBrokenTiles() {
         return numBrokenTiles;
-    }
-
-    public int getNumBatteries() {
-        return numBatteries;
-    }
-
-    public int getNumAliens() {
-        return crewMap.get(CrewType.PINK_ALIEN) + crewMap.get(CrewType.BROWN_ALIEN);
-    }
-
-    public int getNumCrewByType(CrewType type) {
-        return crewMap.get(type);
-    }
-
-    public int getNumCrew() {
-        return crewMap.get(CrewType.PINK_ALIEN) + crewMap.get(CrewType.BROWN_ALIEN) + crewMap.get(CrewType.HUMAN);
-    }
-
-    public int getNumExposedConnectors() {
-        return numExposedConnectors;
-    }
-
-    public List<Direction> getProtectedDirections() {
-        return protectedDirections;
-    }
-
-    public List<Tile> getTilesBuffer() {
-        return tilesBuffer;
-    }
-
-    public void addTileInBuffer(Tile tile) {
-        tilesBuffer.add(tile);
-    }
-
-    // do we need this?
-    public Map<BoxType, Integer> getBoxes() {
-        return boxes;
-    }
-
-    public int getBoxes(BoxType type) {
-        return boxes.get(type);
-    }
-
-    public double getBaseFirePower() {
-        return baseFirePower;
-    }
-
-    public int getBasePropulsionPower() {
-        return basePropulsionPower;
-    }
-
-    public void removeCrewByType(CrewType type) {
-
-        // todo: handle this gracefully
-        assert crewMap.containsKey(type);
-        assert crewMap.get(type) > 0;
-
-        crewMap.put(type, this.getNumCrewByType(type) - 1);
-    }
-
-    public void removeBatteries(int lostBatteries) {
-        assert this.getNumBatteries() - lostBatteries >= 0;
-        numBatteries -= lostBatteries;
-    }
-
-    // this method remove 'lostBoxes' boxes, prioritizing high value boxes, then removes battery. As stated in the game rules
-    // todo: this method only updates the new boxes map, does not remove box from a storageTile!!
-    public void removeBoxes(int lostBoxes) {
-        int yetToRemove = lostBoxes;
-        List<BoxType> sortedTypes = this.boxes.keySet().stream().sorted(Comparator.comparing(BoxType::getPriority).reversed()).toList();
-
-        for (BoxType type : sortedTypes) {
-            if (yetToRemove > 0) {
-                yetToRemove -= this.removeBoxes(type, yetToRemove);
-            }
-        }
-
-        if (yetToRemove > 0) {
-            this.removeBatteries(yetToRemove);
-        }
-    }
-
-    // returns the number of boxes that were removed
-    // todo: add storageTile location from the matrix to remove boxes from the tile as well?
-    public int removeBoxes(BoxType type, int lostBoxes) {
-        Integer currentCount = this.boxes.get(type);
-
-        if (currentCount == null) {
-            return 0;
-        }
-
-        if (currentCount >= lostBoxes) {
-            this.boxes.put(type, currentCount - lostBoxes);
-            return lostBoxes;
-        }
-
-        this.boxes.put(type, 0);
-        return currentCount;
-    }
-
-    public boolean isShipLegal() {
-
-        // for all tiles, existing neighbouring tiles must have matching connector
-        for (int i = 0; i < shipHeight; i++) {
-            for (int j = 0; j < shipWidth; j++) {
-
-                if (tilesMatrix[i][j] != null) {
-                    Tile currTile = tilesMatrix[i][j];
-
-                    if (i != 0 && !currTile.isValidConnection(Direction.UP, tilesMatrix[i - 1][j])) {
-                        return false;
-                    }
-
-                    if (j != 0 && !currTile.isValidConnection(Direction.LEFT, tilesMatrix[i][j - 1])) {
-                        return false;
-                    }
-
-                    if (i != shipHeight - 1 && !currTile.isValidConnection(Direction.DOWN, tilesMatrix[i + 1][j])) {
-                        return false;
-                    }
-
-                    if (j != shipWidth - 1 && !currTile.isValidConnection(Direction.RIGHT, tilesMatrix[i][j + 1])) {
-                        return false;
-                    }
-                }
-            }
-        }
-
-        // todo: check that all propulsors are pointing backwards
-
-        return false;
-    }
-
-    public int calcTotalPropulsionPower(int usedBatteries) {
-        return -1;
-    }
-
-    public int calcTotalFirePower(int usedBatteries) {
-        return -1;
-    }
-
-    // the method should return true if tile was broken, so state of the ship can be updated
-    // k is height/width of the attack depending on dir
-    public boolean handleMeteor(Direction dir, Meteor meteor, int k) {
-        return true;
-    }
-
-    // the method should return true if tile was broken, so state of the ship can be updated
-    // k is height/width of the attack depending on dir
-    public boolean handleShot(Direction dir, Shot shot, int k) {
-        return true;
     }
 
     public boolean placeTile(Tile tile, int x, int y) {
@@ -309,6 +160,116 @@ public class Ship {
         this.updateExposedConnectors();
     }
 
+    public List<Tile> getTilesBuffer() {
+        return tilesBuffer;
+    }
+
+    public void addTileInBuffer(Tile tile) {
+        tilesBuffer.add(tile);
+    }
+
+
+    // ship resources
+    public int getNumBatteries() {
+        return numBatteries;
+    }
+
+    public int getNumCrewByType(CrewType type) {
+        return crewMap.get(type);
+    }
+
+    public int getNumCrew() {
+        return crewMap.get(CrewType.PINK_ALIEN) + crewMap.get(CrewType.BROWN_ALIEN) + crewMap.get(CrewType.HUMAN);
+    }
+
+    public int getNumAliens() {
+        return crewMap.get(CrewType.PINK_ALIEN) + crewMap.get(CrewType.BROWN_ALIEN);
+    }
+
+    // do we need this?
+    public Map<BoxType, Integer> getBoxes() {
+        return boxes;
+    }
+
+    public int getBoxes(BoxType type) {
+        return boxes.get(type);
+    }
+
+    public void removeBatteries(int lostBatteries) {
+        assert this.getNumBatteries() - lostBatteries >= 0;
+        numBatteries -= lostBatteries;
+    }
+
+    // this method remove 'lostBoxes' boxes, prioritizing high value boxes, then removes battery. As stated in the game rules
+    // todo: this method only updates the new boxes map, does not remove box from a storageTile!!
+    public void removeBoxes(int lostBoxes) {
+        int yetToRemove = lostBoxes;
+        List<BoxType> sortedTypes = this.boxes.keySet().stream().sorted(Comparator.comparing(BoxType::getPriority).reversed()).toList();
+
+        for (BoxType type : sortedTypes) {
+            if (yetToRemove > 0) {
+                yetToRemove -= this.removeBoxes(type, yetToRemove);
+            }
+        }
+
+        if (yetToRemove > 0) {
+            this.removeBatteries(yetToRemove);
+        }
+    }
+
+    // returns the number of boxes that were removed
+    // todo: add storageTile location from the matrix to remove boxes from the tile as well?
+    public int removeBoxes(BoxType type, int lostBoxes) {
+        Integer currentCount = this.boxes.get(type);
+
+        if (currentCount == null) {
+            return 0;
+        }
+
+        if (currentCount >= lostBoxes) {
+            this.boxes.put(type, currentCount - lostBoxes);
+            return lostBoxes;
+        }
+
+        this.boxes.put(type, 0);
+        return currentCount;
+    }
+
+    public void removeCrewByType(CrewType type) {
+
+        // todo: handle this gracefully
+        assert crewMap.containsKey(type);
+        assert crewMap.get(type) > 0;
+
+        crewMap.put(type, this.getNumCrewByType(type) - 1);
+    }
+
+
+    // stats
+    public int getNumExposedConnectors() {
+        return numExposedConnectors;
+    }
+
+    public List<Direction> getProtectedDirections() {
+        return protectedDirections;
+    }
+
+    public double getBaseFirePower() {
+        return baseFirePower;
+    }
+
+    public int getBasePropulsionPower() {
+        return basePropulsionPower;
+    }
+
+    public int calcTotalPropulsionPower(int usedBatteries) {
+        return -1;
+    }
+
+    public int calcTotalFirePower(int usedBatteries) {
+        return -1;
+    }
+
     public void updateBaseFirePower(double i) {
         baseFirePower = baseFirePower + i;
     }
@@ -317,22 +278,8 @@ public class Ship {
         basePropulsionPower = basePropulsionPower + i;
     }
 
-    // todo: check if works
-    public void removeProtectedDirections(Set<Direction> dir) {
-        for (Direction d : dir) {
-            for (Direction direction : protectedDirections) {
-                if (d.equals(direction)) {
-                    protectedDirections.remove(d);
-                    break;
-                }
-            }
-        }
-    }
-
     public void updateExposedConnectors(){
         int cont = 0;
-        // set of connections that increases the count, used to make code more readable
-        Set<Connection> connectors = new HashSet<>(Set.of(Connection.SINGLE, Connection.DOUBLE, Connection.UNIVERSAL));
         for(int i = 0; i<shipHeight; i++) {
             for(int j = 0; j<shipWidth; j++) {
                 if (tilesMatrix[i][j] != null) {
@@ -367,5 +314,64 @@ public class Ship {
             }
         }
         this.numExposedConnectors = cont;
+    }
+
+    // todo: check if works
+    public void removeProtectedDirections(Set<Direction> dir) {
+        for (Direction d : dir) {
+            for (Direction direction : protectedDirections) {
+                if (d.equals(direction)) {
+                    protectedDirections.remove(d);
+                    break;
+                }
+            }
+        }
+    }
+
+
+    // events and checks
+    public boolean isShipLegal() {
+
+        // for all tiles, existing neighbouring tiles must have matching connector
+        for (int i = 0; i < shipHeight; i++) {
+            for (int j = 0; j < shipWidth; j++) {
+
+                if (tilesMatrix[i][j] != null) {
+                    Tile currTile = tilesMatrix[i][j];
+
+                    if (i != 0 && !currTile.isValidConnection(Direction.UP, tilesMatrix[i - 1][j])) {
+                        return false;
+                    }
+
+                    if (j != 0 && !currTile.isValidConnection(Direction.LEFT, tilesMatrix[i][j - 1])) {
+                        return false;
+                    }
+
+                    if (i != shipHeight - 1 && !currTile.isValidConnection(Direction.DOWN, tilesMatrix[i + 1][j])) {
+                        return false;
+                    }
+
+                    if (j != shipWidth - 1 && !currTile.isValidConnection(Direction.RIGHT, tilesMatrix[i][j + 1])) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        // todo: check that all propulsors are pointing backwards
+
+        return false;
+    }
+
+    // the method should return true if tile was broken, so state of the ship can be updated
+    // k is height/width of the attack depending on dir
+    public boolean handleMeteor(Direction dir, Meteor meteor, int k) {
+        return true;
+    }
+
+    // the method should return true if tile was broken, so state of the ship can be updated
+    // k is height/width of the attack depending on dir
+    public boolean handleShot(Direction dir, Shot shot, int k) {
+        return true;
     }
 }
