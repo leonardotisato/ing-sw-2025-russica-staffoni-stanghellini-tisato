e85c507683e4d7eb1245484a937ff7705a4ab71e
Filippo <filippostaffoni@hotmail.it>
2025-03-18 16:06:22 +0100

    tested the cardLoader, working with all card types

--- src/test/java/it/polimi/ingsw/cg04/model/resources/CardLoaderTest.java
+++ src/test/java/it/polimi/ingsw/cg04/model/resources/CardLoaderTest.java
@@ -1,14 +1,13 @@
 package it.polimi.ingsw.cg04.model.resources;
 
-import it.polimi.ingsw.cg04.model.adventureCards.AbandonedShip;
-import it.polimi.ingsw.cg04.model.adventureCards.AbandonedStation;
-import it.polimi.ingsw.cg04.model.adventureCards.AdventureCard;
-import it.polimi.ingsw.cg04.model.adventureCards.MeteorsRain;
+import it.polimi.ingsw.cg04.model.adventureCards.*;
 import it.polimi.ingsw.cg04.model.enumerations.BoxType;
 import it.polimi.ingsw.cg04.model.enumerations.Connection;
 import it.polimi.ingsw.cg04.model.enumerations.Direction;
 import it.polimi.ingsw.cg04.model.enumerations.Meteor;
+import it.polimi.ingsw.cg04.model.adventureCards.*;
 import it.polimi.ingsw.cg04.model.utils.CardLoader;
+import it.polimi.ingsw.cg04.model.utils.TileLoader;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -81,6 +80,32 @@ class CardLoaderTest {
         assertEquals(List.of(Meteor.HEAVYMETEOR, Meteor.HEAVYMETEOR), meteorsRain.getMeteors());
 
     }
+    
+    @Test
+    void LoadAllFromJson() {
+        // Carica il JSON di test
+        List<Integer> cards1 = new ArrayList<>();
+        List<Integer> cards2 = new ArrayList<>();
+        Map<Integer, AdventureCard> cards = CardLoader.loadCardsFromJson("src/test/java/it/polimi/ingsw/cg04/model/resources/AdventureCardsFile.json", cards1, cards2);
 
+        assertNotNull(cards);
+        assertNotNull(cards1);
+        assertNotNull(cards2);
+        assertFalse(cards.isEmpty());
+        assertFalse(cards1.isEmpty());
+        assertFalse(cards2.isEmpty());
+
+        assertInstanceOf(AbandonedShip.class, cards.get(1));
+        assertInstanceOf(AbandonedStation.class, cards.get(2));
+        assertInstanceOf(Epidemic.class, cards.get(3));
+        assertInstanceOf(MeteorsRain.class, cards.get(4));
+        assertInstanceOf(OpenSpace.class, cards.get(5));
+        assertInstanceOf(Pirates.class, cards.get(6));
+        assertInstanceOf(Planets.class, cards.get(7));
+        assertInstanceOf(Slavers.class, cards.get(8));
+        assertInstanceOf(Smugglers.class, cards.get(9));
+        assertInstanceOf(Stardust.class, cards.get(10));
+        assertInstanceOf(WarZone.class, cards.get(11));
+    }
 
 }
