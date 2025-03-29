df30873d03fb0c22b69daa2221482211688defb8
Leonardo <10844472@polimi.it>
2025-03-29 19:01:53 +0100

    updated player state

--- src/main/java/it/polimi/ingsw/cg04/model/enumerations/PlayerState.java
+++ src/main/java/it/polimi/ingsw/cg04/model/enumerations/PlayerState.java
@@ -1,10 +1,25 @@
 package it.polimi.ingsw.cg04.model.enumerations;
 
 public enum PlayerState {
+    PRE_LOBBY,
+    LOBBY,
+    READY,
+    PICKING,
+    SHOWING_FACE_UP,
+    PLACING,
+    LOOKING_PILE,
+    FLIGHT,
+    DECIDING,
+    CHOOSE_PLANET,
+    CHOOSE_CANNON,
+    CHOOSE_PROPULSION,
+    CHOOSE_SHIELD,
+    HANDLE_RESOURCES,
+    HANDLE_CREW,
+    FIX_SHIP,
+    WAIT,
+    // FLYING,
     RETIRED,
     LOST,
-    BUILDING,
-    IN_LOBBY,
-    SHIP_CORRECTION,
-    FLIGHT
+    FINISH,
 }
