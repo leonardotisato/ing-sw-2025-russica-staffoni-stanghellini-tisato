c4641bc92fae375c1e290fc1ffd1560ad1d78d34
Leonardo <10844472@polimi.it>
2025-04-03 14:36:27 +0200

    template for player card actions

--- src/main/java/it/polimi/ingsw/cg04/model/GameStates/PlanetsState.java
+++ src/main/java/it/polimi/ingsw/cg04/model/GameStates/PlanetsState.java
@@ -3,7 +3,11 @@ package it.polimi.ingsw.cg04.model.GameStates;
 import it.polimi.ingsw.cg04.model.Player;
 import it.polimi.ingsw.cg04.model.PlayerActions.PlayerAction;
 
+import java.util.Map;
+
 public class PlanetsState extends AdventureCardState {
+    Map<Player, Integer> choosenPlanet;
+
     public void handleAction(Player player, PlayerAction action) {
         action.execute(player);
     }
