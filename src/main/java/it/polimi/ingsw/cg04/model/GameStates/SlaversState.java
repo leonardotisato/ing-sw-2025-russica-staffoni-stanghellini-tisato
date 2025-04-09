c754568db0e2cc834d4e35a3e4273c2e31c46167
Daniele Russica <daniele.russica@gmail.com>
2025-04-09 17:03:15 +0200

    untested CompareFirePowerAction and SlaverState

--- src/main/java/it/polimi/ingsw/cg04/model/GameStates/SlaversState.java
+++ src/main/java/it/polimi/ingsw/cg04/model/GameStates/SlaversState.java
@@ -3,15 +3,180 @@ package it.polimi.ingsw.cg04.model.GameStates;
 import it.polimi.ingsw.cg04.model.Game;
 import it.polimi.ingsw.cg04.model.Player;
 import it.polimi.ingsw.cg04.model.PlayerActions.PlayerAction;
-import it.polimi.ingsw.cg04.model.adventureCards.AdventureCard;
+import it.polimi.ingsw.cg04.model.Ship;
+import it.polimi.ingsw.cg04.model.enumerations.Connection;
+import it.polimi.ingsw.cg04.model.enumerations.Direction;
+import it.polimi.ingsw.cg04.model.tiles.Tile;
+import it.polimi.ingsw.cg04.model.utils.Coordinates;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SlaversState extends AdventureCardState {
+
+
+    private boolean isOpponentDead = false;
+
+    private final int opponentFirePower;
+    private final int reward;
+    private final int delta;
+    private final int crewLost;
+
+    // states the player can be in: WAITING TO BE ATTACKED, ACTIVATE_CANNONS, REMOVE_CREW, DECIDE_REWARD, DONE
+    private final int WAIT = 0;
+    private final int ACTIVATE_CANNONS = 1;
+    private final int REMOVE_CREW = 2;
+    private final int DECIDE_REWARD = 3;
+    private final int DONE = 4;
+
+    private final List<Integer> playerStates = new ArrayList<>();
+
     public SlaversState(Game game) {
         super(game);
+
+        this.opponentFirePower = card.getFirePower();
+        this.reward = card.getEarnedCredits();
+        this.delta = card.getDaysLost();
+        this.crewLost = card.getLostMembers();
+
+        for (int i = 0; i < game.getPlayers().size(); i++) {
+            playerStates.add(WAIT);
+        }
+
+        // first leader must compare his firepower with opponents
+        playerStates.set(0, ACTIVATE_CANNONS);
     }
+
     public void handleAction(Player player, PlayerAction action) {
-        action.execute(player);
+        // todo: delete me!
+    }
+
+    public void getReward(Player player, boolean acceptReward) {
+        int playerIdx = sortedPlayers.indexOf(player);
+
+        if (playerStates.get(playerIdx) == DECIDE_REWARD && isOpponentDead) {
+            if (acceptReward) {
+                // give reward to winner
+                player.updateCredits(reward);
+                player.move(-delta);
+                playerStates.set(playerIdx, DONE);
+            } else {
+                // player won but declined reward
+                playerStates.set(playerIdx, DONE);
+            }
+
+            // check if every one is done
+            if (isAllDone(playerStates)) {
+                // transition to next adventure card
+                triggerNextState();
+            }
+        }
+
+    }
+
+    public void compareFirePower(Player player, List<Coordinates> batteries, List<Coordinates> doubleCannons) {
+        int playerIdx = sortedPlayers.indexOf(player);
+        Ship ship = player.getShip();
+
+        if (playerStates.get(playerIdx) == ACTIVATE_CANNONS) {
+
+            // remove batteries used
+            for (Coordinates c : batteries) {
+                ship.removeBatteries(1, c.getX(), c.getY());
+            }
+
+            // increase firePower adding new double cannons based on orientation
+            double firePower = ship.getBaseFirePower();
+
+            for (Coordinates c : doubleCannons) {
+                // if laser is up bonus is 2
+                if (ship.getTile(c.getX(), c.getY()).getConnection(Direction.UP) == Connection.GUN) {
+                    firePower += 2;
+                } else {
+                    //else bonus is 1
+                    firePower += 1;
+                }
+            }
+
+            // now check if player defeated the opponent
+
+            // player has lost
+            if (firePower < opponentFirePower) {
+                playerStates.set(playerIdx, REMOVE_CREW);
+
+                // now playerIdx+1 needs to activate his cannons
+                if (playerIdx + 1 < playerStates.size()) {
+                    playerStates.set(playerIdx + 1, ACTIVATE_CANNONS);
+                }
+            }
+
+            // tie, player does not get reward and simply end his turn
+            if (firePower == opponentFirePower) {
+                playerStates.set(playerIdx, DONE);
+            }
+
+            if (firePower > opponentFirePower) {
+                isOpponentDead = true;
+                playerStates.set(playerIdx, DECIDE_REWARD);
+
+                // set all players in WAIT state to DONE state, they won't need to face opponent
+                for (int i = 0; i < playerStates.size(); i++) {
+                    if (playerStates.get(i) == WAIT) {
+                        playerStates.set(i, DONE);
+                    }
+                }
+            }
+
+            // check if every one is done
+            if (isAllDone(playerStates)) {
+                // transition to next adventure card
+                triggerNextState();
+            }
+        }
+    }
+
+    public void removeCrew(Player player, List<Coordinates> coordinates, List<Integer> numCrewMembersLost) {
+        int playerIdx = sortedPlayers.indexOf(player);
+
+        if (playerStates.get(playerIdx) == REMOVE_CREW) {
+
+            // if invalid input do nothing
+            if (coordinates == null && numCrewMembersLost == null) {
+                System.out.println("Remove crew is mandatory!");
+            }
+
+            // check that total sacrificed is <= to crewLost
+            assert numCrewMembersLost != null;
+            int totalCrewMembers = numCrewMembersLost.stream().mapToInt(Integer::intValue).sum();
+            if (totalCrewMembers < crewLost) {
+                System.out.println("Abort handleCrew: " + player.getName() + " did not provide enough members");
+                return;
+            }
+
+            // remove crew members provided
+            for (int i = 0; i < coordinates.size(); i++) {
+                Tile currTile = player.getShip().getTile(coordinates.get(i).getX(), coordinates.get(i).getY());
+                player.getShip().removeCrew(currTile.getHostedCrewType(), coordinates.get(i).getX(), coordinates.get(i).getY(), numCrewMembersLost.get(i));
+            }
+
+            // player lost crew and he is done for the adventure
+            playerStates.set(playerIdx, DONE);
+
+            // check if every one is done
+            if (isAllDone(playerStates)) {
+                // transition to next adventure card
+                triggerNextState();
+            }
+        }
+    }
+
+
+    private boolean isAllDone(List<Integer> list) {
+        for (Integer integer : list) {
+            if (integer != DONE) {
+                return false;
+            }
+        }
+        return true;
     }
 }
