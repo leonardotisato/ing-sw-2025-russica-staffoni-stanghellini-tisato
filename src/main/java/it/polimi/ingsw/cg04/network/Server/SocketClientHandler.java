dcc80ba222c3a46c12f1de45b75bddc7c753098b
Daniele Russica <daniele.russica@gmail.com>
2025-04-24 09:05:56 +0200

    added thread to handle action

--- src/main/java/it/polimi/ingsw/cg04/network/Server/SocketClientHandler.java
+++ src/main/java/it/polimi/ingsw/cg04/network/Server/SocketClientHandler.java
@@ -2,17 +2,19 @@ package it.polimi.ingsw.cg04.network.Server;
 
 import it.polimi.ingsw.cg04.controller.GamesController;
 import it.polimi.ingsw.cg04.model.PlayerActions.Action;
-import it.polimi.ingsw.cg04.model.PlayerActions.PlayerAction;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class SocketClientHandler extends ClientHandler implements Runnable {
 
     private final Socket socket;
     private final ObjectOutputStream out;
+    private final ExecutorService inputHandler = Executors.newSingleThreadExecutor();
 
     public SocketClientHandler(GamesController controller, Server server, Socket socket) throws IOException {
         super(controller, server);
@@ -37,11 +39,14 @@ public class SocketClientHandler extends ClientHandler implements Runnable {
             try {
                 Action action = (Action) in.readObject();
 
-                // let the controller handle the action... naive, but make it work first!
-                action.dispatchTo(controller);
+                inputHandler.submit(() -> handleInput(action));
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    public void handleInput(Action action) {
+        action.dispatchTo(controller);
     }
 }
