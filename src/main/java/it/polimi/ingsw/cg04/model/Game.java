a9ab2075efe0a74250c06177e6cc4036eae5a33f
Filippo Staffoni <filippostaffoni@hotmail.it>
2025-06-24 19:01:21 +0200

    nada

--- src/main/java/it/polimi/ingsw/cg04/model/Game.java
+++ src/main/java/it/polimi/ingsw/cg04/model/Game.java
@@ -390,8 +390,7 @@ public class Game implements Serializable {
      */
     public AdventureCard getNextAdventureCard() {
         if (this.adventureCardsDeck.isEmpty()) return null;
-        //this.currentAdventureCard = this.adventureCardsMap.get(adventureCardsDeck.removeFirst());
-        this.currentAdventureCard = this.adventureCardsMap.get(12);
+        this.currentAdventureCard = this.adventureCardsMap.get(adventureCardsDeck.removeFirst());
         return this.currentAdventureCard;
     }
 
